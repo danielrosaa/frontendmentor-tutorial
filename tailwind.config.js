561a79b5a1b77cc022433dd07b9111416e9cb7fc
Daniel Rosa <danielrosaa@gmail.com>
2024-08-21 15:13:16 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        slate: {
+            300: '#D5E1EF',
+            500: '#68778D',
+            900: '#1F314F'
+          },
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif']
+      },
+    },
+  }
